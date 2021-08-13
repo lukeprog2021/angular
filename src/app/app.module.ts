bdf162f09eb062ebea6787130302d7306eb80b39
lukeprog2021 <82843331+lukeprog2021@users.noreply.github.com>
2021-08-12 22:00:20 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import {CursosModule} from "./cursos/cursos.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
